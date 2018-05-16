4840fa03107e9dd2baa31b7216f54bfe5b617967
zukonake <zukonake@protonmail.com>
2018-05-16 21:26:56 +0200

    Updated linear classes, added hash functions

--- src/linear/point_2d.hpp
+++ src/linear/point_2d.hpp
@@ -1,6 +1,31 @@
 #pragma once
 
-#include <glm/detail/tvec2.hpp>
+#include <functional>
+//
+#include <glm/detail/type_vec2.hpp>
+
+namespace linear
+{
 
 template<typename T>
-using Point2d = glm::detail::tvec2<T>;
+using Point2d = glm::tvec2<T>;
+
+}
+
+namespace std
+{
+
+using namespace linear;
+
+template<typename T>
+struct hash<Point2d<T>>
+{
+    size_t operator()(Point2d<T> const &k) const
+    {
+        return ((hash<T>()(k.x) ^
+                (hash<T>()(k.y) << 1))>> 1);
+    }
+};
+
+}
+
--- src/linear/point_3d.hpp
+++ src/linear/point_3d.hpp
@@ -1,6 +1,31 @@
 #pragma once
 
-#include <glm/detail/tvec3.hpp>
+#include <functional>
+//
+#include <glm/detail/type_vec3.hpp>
+
+namespace linear
+{
 
 template<typename T>
-using Point2d = glm::detail::tvec3<T>;
+using Point3d = glm::tvec3<T>;
+
+}
+
+namespace std
+{
+
+using namespace linear;
+
+template<typename T>
+struct hash<Point3d<T>>
+{
+    size_t operator()(Point3d<T> const &k) const
+    {
+        return (((hash<T>()(k.x) ^
+                 (hash<T>()(k.y) << 1))>> 1) ^
+                 (hash<T>()(k.z) << 1))>> 1;
+    }
+};
+
+}
