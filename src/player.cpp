38b3000554161d51fa8a518e16da3b1b5b7885bb
zukonake <zukonake@protonmail.com>
2018-06-09 05:23:54 +0200

    Better TileState sending function

--- src/player.cpp
+++ src/player.cpp
@@ -22,18 +22,25 @@ void Player::receive(ENetPacket *packet)
     view_size = client_data.view_size;
 }
 
+#include <iostream>
+
 ENetPacket *Player::send() const
 {
     //TODO store buffer in Player?
     Vector<net::TileState> tiles;
     tiles.reserve(view_size.x * view_size.y);
-    for(SizeT i = 0; i < (SizeT)(view_size.x * view_size.y); ++i)
+    world::MapPoint offset;
+    world::MapPoint entity_pos = (world::MapPoint)entity->get_pos();
+    offset.z = 0;
+    for(offset.y = -(int)(view_size.y / 2); offset.y <= view_size.y / 2; ++offset.y)
     {
-        world::MapPoint offset = {i % view_size.x, i / view_size.x, 0};
-        world::MapPoint tile_pos = (world::MapPoint)entity->get_pos() + offset;
-        auto const &tile_type = entity->world[tile_pos].type;
-        tiles.emplace_back((net::TileState::Shape)tile_type->shape,
-                           tile_type->tex_pos);
+        for(offset.x = -(int)(view_size.x / 2); offset.x <= view_size.x / 2; ++offset.x)
+        {
+            world::MapPoint tile_pos = entity_pos + offset;
+            auto const &tile_type = entity->world[tile_pos].type;
+            tiles.emplace_back((net::TileState::Shape)tile_type->shape,
+                               tile_type->tex_pos);
+        }
     }
     net::ServerData server_data = {tiles};
     Vector<U8> bytes;
