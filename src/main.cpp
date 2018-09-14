103bd5d1b5e71be53b5a8e1357ed04f0b27688f9
zukonake <zukonake@protonmail.com>
2018-09-14 02:12:32 +0200

    Removed gotos from add_client

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,9 @@ void erase_client(Uns id) {
 }
 
 void kick_peer(ENetPeer *peer) {
+    U8* ip = (U8*)&peer->address.host;
+    LUX_LOG("terminating connection with %u.%u.%u.%u",
+            ip[0], ip[1], ip[2], ip[3]);
     enet_peer_disconnect(peer, 0);
     enet_host_flush(server.host);
     enet_peer_reset(peer);
@@ -55,7 +58,7 @@ void kick_client(String const& name, String const& reason) {
     erase_client(client_id);
 }
 
-void add_client(ENetPeer* peer) {
+int add_client(ENetPeer* peer) {
     U8* ip = (U8*)&peer->address.host;
     static_assert(sizeof(peer->address.host) == 4);
     LUX_LOG("new client %u.%u.%u.%u connecting", ip[0], ip[1], ip[2], ip[3]);
@@ -75,103 +78,94 @@ void add_client(ENetPeer* peer) {
         init_pack = enet_peer_receive(peer, &channel_id);
         if(tries >= MAX_TRIES) {
             LUX_LOG("client did not send an init packet");
-            goto client_init_fail;
+            return -1;
         }
         //@RESEARCH whether an unsequenced packet can arrive before a reliable
         //one, if so, we need to ignore tick packets here
         if(init_pack != nullptr && channel_id != INIT_CHANNEL) {
             LUX_LOG("client sent a packet on unexcepted channel %u"
                     ", we expected it on channel %u", channel_id, INIT_CHANNEL);
-            goto client_init_fail;
+            return -1;
         }
         ++tries;
         std::this_thread::sleep_for(TRY_TIME);
     }
     LUX_LOG("received init packet after %zu", tries);
 
-    ///we enter a new scope to permit gotos to work
+    ///we are gonna do a direct copy, so we disable padding,
+    ///no need to reverse the byte order,
+    ///because we are not using anything bigger than a byte right now,
+    //@CONSIDER adding manual padding to increase efficiency
+    #pragma pack(push, 1)
+    struct
     {
-        ///we are gonna do a direct copy, so we disable padding,
-        ///no need to reverse the byte order,
-        ///because we are not using anything bigger than a byte right now,
-        //@CONSIDER adding manual padding to increase efficiency
-        #pragma pack(push, 1)
-        struct
-        {
-            Arr<U8, 3> ver;
-            Arr<U8, CLIENT_NAME_LEN> name;
-        } client_init_data;
-        #pragma pack(pop)
+        Arr<U8, 3> ver;
+        Arr<U8, CLIENT_NAME_LEN> name;
+    } client_init_data;
+    #pragma pack(pop)
 
-        if(sizeof(client_init_data) != init_pack->dataLength) {
-            LUX_LOG("client sent invalid init packet with size %zu"
-                    "instead of %zu",
-                    sizeof(client_init_data), init_pack->dataLength);
-            goto client_init_fail;
-        }
-        ///remember, this assumes that there are no struct fields bigger
-        ///than a byte, otherwise we would need to reverse the network order
-        //@CONSIDER using a standarized deserializer function for this
-        std::memcpy((U8*)&client_init_data, init_pack->data,
-                    sizeof(client_init_data));
-        enet_packet_destroy(init_pack);
+    if(sizeof(client_init_data) != init_pack->dataLength) {
+        LUX_LOG("client sent invalid init packet with size %zu"
+                "instead of %zu",
+                sizeof(client_init_data), init_pack->dataLength);
+        return -1;
+    }
+    ///remember, this assumes that there are no struct fields bigger
+    ///than a byte, otherwise we would need to reverse the network order
+    //@CONSIDER using a standarized deserializer function for this
+    std::memcpy((U8*)&client_init_data, init_pack->data,
+                sizeof(client_init_data));
+    enet_packet_destroy(init_pack);
 
-        static_assert(sizeof(client_init_data.ver[0]) ==
-                      sizeof(LUX_NET_VERSION_MAJOR));
-        static_assert(sizeof(client_init_data.ver[1]) ==
-                      sizeof(LUX_NET_VERSION_MINOR));
-        if(client_init_data.ver[0] != LUX_NET_VERSION_MAJOR) {
-            LUX_LOG("client uses an incompatible major lux net api version"
-                    ", we use %u, they use %u",
-                    LUX_NET_VERSION_MAJOR, client_init_data.ver[0]);
-            goto client_init_fail;
-        }
-        if(client_init_data.ver[1] >  LUX_NET_VERSION_MINOR) {
-            LUX_LOG("client uses a newer minor lux net api version"
-                    ", we use %u, they use %u",
-                    LUX_NET_VERSION_MINOR, client_init_data.ver[1]);
-            goto client_init_fail;
-        }
-
-        //@CONSIDER putting it outside function scope, as this struct gets
-        //reused for every client connection, maybe even do it for the packet
-        #pragma pack(push, 1)
-        struct
-        {
-            Arr<U8, SERVER_NAME_LEN> name = conf.name;
-        } server_init_data;
-        #pragma pack(pop)
-
-        ENetPacket* server_init_pack =
-            enet_packet_create((U8*)&server_init_data,
-            sizeof(server_init_data), ENET_PACKET_FLAG_RELIABLE);
-        if(server_init_pack == nullptr) {
-            LUX_LOG("failed to create server init packet");
-            goto client_init_fail;
-        }
-        if(enet_peer_send(peer, INIT_CHANNEL, server_init_pack) < 0) {
-            LUX_LOG("failed to send server init packet");
-            enet_packet_destroy(server_init_pack);
-            return;
-        }
-        enet_host_flush(server.host);
-        enet_packet_destroy(server_init_pack);
-
-        Server::Client* client = &server.clients.emplace_back();
-        client->peer = peer;
-        client->peer->data = (void*)(server.clients.size() - 1);
-        client->name = String((char const*)client_init_data.name.data());
-
-        LUX_LOG("client %s connected successfully", client->name.c_str());
-        //@TODO set entity
-        return;
+    static_assert(sizeof(client_init_data.ver[0]) ==
+                  sizeof(LUX_NET_VERSION_MAJOR));
+    static_assert(sizeof(client_init_data.ver[1]) ==
+                  sizeof(LUX_NET_VERSION_MINOR));
+    if(client_init_data.ver[0] != LUX_NET_VERSION_MAJOR) {
+        LUX_LOG("client uses an incompatible major lux net api version"
+                ", we use %u, they use %u",
+                LUX_NET_VERSION_MAJOR, client_init_data.ver[0]);
+        return -1;
+    }
+    if(client_init_data.ver[1] >  LUX_NET_VERSION_MINOR) {
+        LUX_LOG("client uses a newer minor lux net api version"
+                ", we use %u, they use %u",
+                LUX_NET_VERSION_MINOR, client_init_data.ver[1]);
+        return -1;
     }
 
-client_init_fail:
-    LUX_LOG("terminating connection with %u.%u.%u.%u",
-            ip[0], ip[1], ip[2], ip[3]);
-    kick_peer(peer);
-    return;
+    //@CONSIDER putting it outside function scope, as this struct gets
+    //reused for every client connection, maybe even do it for the packet
+    #pragma pack(push, 1)
+    struct
+    {
+        Arr<U8, SERVER_NAME_LEN> name = conf.name;
+    } server_init_data;
+    #pragma pack(pop)
+
+    ENetPacket* server_init_pack =
+        enet_packet_create((U8*)&server_init_data,
+        sizeof(server_init_data), ENET_PACKET_FLAG_RELIABLE);
+    if(server_init_pack == nullptr) {
+        LUX_LOG("failed to create server init packet");
+        return -1;
+    }
+    if(enet_peer_send(peer, INIT_CHANNEL, server_init_pack) < 0) {
+        LUX_LOG("failed to send server init packet");
+        enet_packet_destroy(server_init_pack);
+        return -1;
+    }
+    enet_host_flush(server.host);
+    enet_packet_destroy(server_init_pack);
+
+    Server::Client* client = &server.clients.emplace_back();
+    client->peer = peer;
+    client->peer->data = (void*)(server.clients.size() - 1);
+    client->name = String((char const*)client_init_data.name.data());
+
+    LUX_LOG("client %s connected successfully", client->name.c_str());
+    //@TODO set entity
+    return 0;
 }
 
 void server_main() {
@@ -194,7 +188,9 @@ void server_main() {
 
         while(enet_host_service(server.host, &event, 0) > 0) {
             if(event.type == ENET_EVENT_TYPE_CONNECT) {
-                add_client(event.peer);
+                if(add_client(event.peer) < 0) {
+                    kick_peer(event.peer);
+                }
             } else if(event.type == ENET_EVENT_TYPE_DISCONNECT) {
                 erase_client((Uns)event.peer->data);
             } else if(event.type == ENET_EVENT_TYPE_RECEIVE) {
