841d3b27ff4b657bcd5d799fa760dcea481804f2
zukonake <zukonake@protonmail.com>
2018-10-25 11:17:09 +0200

    Flood-fill lightning system

--- src/map.cpp
+++ src/map.cpp
@@ -12,17 +12,11 @@
 
 static VecMap<ChkPos, Chunk> chunks;
 
-constexpr Uns LIGHTNING_BITS_PER_COLOR = 5;
-static_assert(LIGHTNING_BITS_PER_COLOR * 3 <= sizeof(LightLvl) * 8);
-constexpr Uns LIGHTNING_RANGE          = std::exp2(LIGHTNING_BITS_PER_COLOR);
-constexpr Uns RAY_NUM  = 4 * (2 * LIGHTNING_RANGE - 2);
-constexpr Uns RAY_AREA = std::pow(2 * LIGHTNING_RANGE - 1, 2);
-static Arr<Vec2F, RAY_NUM> ray_step_map;
-//@CONSIDER Arr2 for 2d access
-static Arr<F32, RAY_AREA>  ray_mul_map;
+constexpr Uns LIGHT_BITS_PER_COLOR = 5;
+static_assert(LIGHT_BITS_PER_COLOR * 3 <= sizeof(LightLvl) * 8);
+constexpr Uns LIGHT_RANGE          = std::exp2(LIGHT_BITS_PER_COLOR);
 
-
-static void update_chunk_lightning(ChkPos const &pos, Chunk& chunk);
+static void update_chunk_light(ChkPos const &pos, Chunk& chunk);
 static bool is_chunk_loaded(ChkPos const& pos) {
     return chunks.count(pos) > 0;
 }
@@ -63,13 +57,11 @@ static Chunk& load_chunk(ChkPos const& pos) {
                 }
             }
         }
-        /*if((map_pos.x % 16 != 0 && map_pos.y % 16 != 0) && pos_hash % 30 == 0) {
-            U16 col = std::rand() % 0x1000;
-            add_light_node(to_map_pos(pos, i),
-                {col & 0x00F, (col & 0x0F0) >> 4, (col & 0xF00) >> 8});
-        }*/
+        chunk.light_lvls[i] = 0x0000;
         chunk.voxels[i] = voxel_id;
-        chunk.light_lvls[i] = 0xFFF0;
+        if((map_pos.x % 16 != 0 && map_pos.y % 16 != 0) && rand() % 1000 == 0) {
+            add_light_node(to_map_pos(pos, i), {1.f, 1.f, 1.f});
+        }
     }
     return chunk;
 }
@@ -94,14 +86,14 @@ VoxelType const& get_voxel_type(MapPos const& pos) {
     return db_voxel_type(get_voxel(pos));
 }
 //@CONSIDER separate file for lightsim
-struct LightSource {
+struct LightNode {
     Vec3<U8> col;
-    static_assert(sizeof(col.r) * 8 >= LIGHTNING_BITS_PER_COLOR);
+    ChkIdx   idx;
+    static_assert(sizeof(col.r) * 8 >= LIGHT_BITS_PER_COLOR);
 };
 
-VecMap<ChkPos, HashTable<ChkIdx, LightSource>>                   light_sources;
-VecMap<ChkPos, HashSet<ChkIdx, util::Identity<ChkIdx>>> awaiting_light_sources;
-VecSet<ChkPos>                                          awaiting_light_updates;
+VecMap<ChkPos, Queue<LightNode>> light_nodes;
+VecSet<ChkPos>                   awaiting_light_updates;
 
 void map_tick(DynArr<ChkPos>& light_updated_chunks) {
     LUX_ASSERT(light_updated_chunks.size() == 0);
@@ -111,38 +103,71 @@ void map_tick(DynArr<ChkPos>& light_updated_chunks) {
         }
     }
     for(auto const& update : light_updated_chunks) {
-        update_chunk_lightning(update, chunks.at(update));
+        update_chunk_light(update, chunks.at(update));
         awaiting_light_updates.erase(update);
     }
 }
 
-
-void add_light_source(MapPos const& pos, Vec3F col) {
+void add_light_node(MapPos const& pos, Vec3F const& col) {
     ChkPos chk_pos = to_chk_pos(pos);
     ChkIdx chk_idx = to_chk_idx(pos);
     //@TODO
-    LUX_ASSERT(light_sources.count(chk_pos) == 0 ||
-               light_sources.at(chk_pos).count(chk_idx) == 0);
-    light_sources[chk_pos][chk_idx] = {(Vec3<U8>)(col * Vec3F(255.f))};
-    awaiting_light_sources[chk_pos].insert(chk_idx);
+    light_nodes[chk_pos].push(LightNode{
+        Vec3<U8>(glm::round(col * (F32)LIGHT_RANGE)), to_chk_idx(pos)});
     awaiting_light_updates.insert(chk_pos);
 }
 
-void del_light_source(MapPos const& pos) {
+void del_light_node(MapPos const& pos) {
     //@TODO
     LUX_UNIMPLEMENTED();
 }
 
-static void apply_light_source(MapPos const& src_pos, LightSource& light) {
-    LUX_UNIMPLEMENTED();
-}
+static void update_chunk_light(ChkPos const &pos, Chunk& chunk) {
+    if(light_nodes.count(pos) == 0) return;
 
-static void update_chunk_lightning(ChkPos const &pos, Chunk& chunk) {
-    if(awaiting_light_sources.count(pos) > 0) {
-        for(auto& chk_idx : awaiting_light_sources.at(pos)) {
-            auto& source = light_sources.at(pos).at(chk_idx);
-            apply_light_source(to_map_pos(pos, chk_idx), source);
+    auto &nodes = light_nodes.at(pos);
+    while(!nodes.empty()) {
+        LightNode node = nodes.front();
+        nodes.pop();
+
+        constexpr MapPos offsets[4] =
+            {{-1,  0}, { 1,  0},
+             { 0, -1}, { 0,  1}};
+
+        MapPos base_pos = to_map_pos(pos, node.idx);
+        //@IMPROVE bit-level parallelism
+
+        LightLvl map_lvl = chunk.light_lvls[node.idx];
+        Vec3<U8> map_color = {(map_lvl & 0xF800) >> 11,
+                              (map_lvl & 0x07C0) >>  6,
+                              (map_lvl & 0x003E) >>  1};
+        if(db_voxel_type(chunk.voxels[node.idx]).shape == VoxelType::BLOCK) {
+            node.col = (Vec3<U8>)glm::round(Vec3F(node.col) * 0.25f);
         }
-        awaiting_light_sources.at(pos).clear();
+        auto is_less = glm::lessThan(map_color, node.col);
+        auto atleast_two = glm::greaterThanEqual(node.col, Vec3<U8>(2u));
+        if(glm::any(is_less)) {
+            /* node.col is guaranteed to be non-zero when is_less is true */
+            Vec3<U8> new_color = node.col * (Vec3<U8>)is_less +
+                                 map_color * (Vec3<U8>)(glm::not_(is_less));
+            chunk.light_lvls[node.idx] = (new_color.r << 11) |
+                                         (new_color.g <<  6) |
+                                         (new_color.b <<  1);
+            if(glm::any(atleast_two)) {
+                Vec3<U8> side_color = node.col - (Vec3<U8>)atleast_two;
+                for(auto const &offset : offsets) {
+                    //@TODO don't spread lights through Z if there is floor
+                    MapPos map_pos = base_pos + offset;
+                    ChkPos chk_pos = to_chk_pos(map_pos);
+                    ChkIdx idx     = to_chk_idx(map_pos);
+                    if(chk_pos == pos) {
+                        nodes.push({side_color, idx});
+                    } else {
+                        light_nodes[chk_pos].push({side_color, idx});
+                        awaiting_light_updates.insert(chk_pos);
+                    }
+                }
+            }
+        }
     }
 }
--- src/map.hpp
+++ src/map.hpp
@@ -15,5 +15,5 @@ Chunk const& get_chunk(ChkPos const& pos);
 VoxelId   get_voxel(MapPos const& pos);
 VoxelType const& get_voxel_type(MapPos const& pos);
 
-void add_light_source(MapPos const& pos, Vec3F const& col);
-void del_light_source(MapPos const& pos);
+void add_light_node(MapPos const& pos, Vec3F const& col);
+void del_light_node(MapPos const& pos);
